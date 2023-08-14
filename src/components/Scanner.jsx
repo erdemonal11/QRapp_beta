8c6f52239cb6a31ebfa1b715aac455d75bad98fe
erdemonal11 <erdemonal11@outlook.com>
2023-08-14 08:32:33 +0300

    Update Scanner.jsx

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "../App.css";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 function Scanner() {
-  const [scanResult, setScanResult] = useState(null);  
+  const [scanResult, setScanResult] = useState(null);
+  const [scannerContent, setScannerContent] = useState(null);
+  const scannerRef = useRef(null);
 
   useEffect(() => {
     const scanner = new Html5QrcodeScanner("reader", {
@@ -14,24 +16,53 @@ function Scanner() {
       fps: 5,
     });
 
+    scannerRef.current = scanner;
+
     scanner.render(success, error);
 
     function success(result) {
       scanner.clear();
       setScanResult(result);
+      setScannerContent(scanner.getHtml());
     }
 
     function error(err) {
       console.warn(err);
     }
+
+    return () => {
+      scanner.clear();
+    };
   }, []);
+
+  function handleGoBack() {
+    setScanResult(null);
+    setScannerContent(null);
+    alert("Please reflesh the page.")
+     
+  }
+
+  useEffect(() => {
+    if (scannerContent) {
+      scannerRef.current.render(success, error, scannerContent);
+    }
+  }, [scannerContent]);
 
   return (
     <div className="App2">
       <h1 className="head">QR Scan</h1>
       {scanResult ? (
         <div>
-          Success: <a target="_blank" href={"http://"+scanResult}>{scanResult}</a>
+          Success:{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={"" + scanResult}
+          >
+            {scanResult}
+          </a>
+          <br /> <br />
+          <button onClick={handleGoBack}>Go Back</button>
         </div>
       ) : (
         <div id="reader"></div>
